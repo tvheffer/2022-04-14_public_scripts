f054e312ef1427d983f7a4d7727010829ee35594
tvheffer <69358825+tvheffer@users.noreply.github.com>
2022-04-14 12:16:41 -0400

    Update 2021-01-21_project_setup.R

--- 2021-01-21_project_setup.R
+++ 2021-01-21_project_setup.R
@@ -2,5 +2,3 @@
 folder_names <- c("1_raw_data", "2_output", "3_tables_figures", "4_docs", "5_scripts") #main folders
 sub_folders  <- c( "5_scripts/drafts", "3_tables_figures/drafts", "4_docs/drafts") #sub-folders
 sapply(c(folder_names,sub_folders), dir.create)# create a directory for all elements in the variables
-
-test
